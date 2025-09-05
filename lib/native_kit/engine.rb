d2f5b22855a183a4e6f1e4e63799b86f8a7a8a29
Jonathan Bennett <jonathan@jbennett.me>
2025-09-05 04:40:39 -0400

    fix for loading error

--- lib/native_kit/engine.rb
+++ lib/native_kit/engine.rb
@@ -1,13 +1,13 @@
-# TODO: why are you not autoloading ???
-require_relative "../../app/helpers/native_kit/application_helper"
 require_relative "rails/routes"
 
 module NativeKit
   class Engine < ::Rails::Engine
     isolate_namespace NativeKit
 
-    ActiveSupport.on_load(:action_view) do
-      include NativeKit::ApplicationHelper
+    initializer "native_kit.action_view" do
+      ActiveSupport::Reloader.to_prepare do
+        ActionView::Base.include ::NativeKit::ApplicationHelper
+      end
     end
   end
 end
